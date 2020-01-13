5399cd170e0d79f57eac4950d82f1035dedfde42
unknown <michaelyu678@gmail.com>
2020-01-13 08:10:53 -0800

    test

---
+++ precache-manifest.f88e8574438a879483be3bba0ab4fd8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6724bf5356de8dde76372d68deaec460",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce92c50feabcae3781eb",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "e5317f167c4b4bb29a8f",
+    "url": "/static/js/2.c6019a09.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c6019a09.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ce92c50feabcae3781eb",
+    "url": "/static/js/main.cae6774c.chunk.js"
+  },
+  {
+    "revision": "2cc18f7e38443b1d022b",
+    "url": "/static/js/runtime-main.3add9fc4.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
